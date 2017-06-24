4f3b41a8516604ba258e3d2076135badbf6076ac
Manuj Shinkar <manujshinkar@manujs-MacBook-Pro.local>
2017-06-23 19:29:53 -0700

    adding files

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -48,7 +48,7 @@ UKF::UKF() {
   std_a_ = 2.0;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 0.3;
+  std_yawdd_ = 0.4;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
